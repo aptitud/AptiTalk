8d1c9562841437d9931afbfda5acc3eadeb03f16
Hugo Häggmark <hugo.haggmark@gmail.com>
2014-03-11 07:17:02 +0100

    Initial commit

---
+++ test/dbAccess/users.spec.js
@@ -0,0 +1,53 @@
+var mongoose = require("mongoose");
+var should = require("should");
+var dbAccess = require("../../lib/dbAccess/dbAccess");
+var testHelpers = require("./testHelpers.js");
+
+describe("Users", function () {
+  before(function (done) {
+    testHelpers.connectMongo();
+    done();
+  });
+
+  beforeEach(function (done) {
+    testHelpers.deleteAll();
+    done();
+  });
+
+  after(function (done) {
+    testHelpers.deleteAll();
+    done();
+  });
+
+  describe("When adding a new user", function (done) {
+    it("adds a correct user", function (done) {
+      var googleId = 'https://www.google.com/accounts/o8/id?id=xxx';
+      var name = 'John Doe';
+      var email = 'john.doe@unknown.com';
+      dbAccess.addUser(googleId, name, email, function (result) {
+        testHelpers.validateOkResult(result);
+        should.exists(result.data._id);
+        result.data.identifier.should.be.equal(googleId);
+        result.data.name.should.be.equal(name);
+        result.data.email.should.be.equal(email);
+        done();
+      });
+    });
+
+    it("if user already exists then it should not add a new user", function (done) {
+      var googleId = 'https://www.google.com/accounts/o8/id?id=xxx';
+      var name = 'John Doe';
+      var email = 'john.doe@unknown.com';
+      dbAccess.addUser(googleId, name, email, function (result) {
+        dbAccess.addUser(googleId, name, email, function (result) {
+          dbAccess.getAllUsers(function (users) {
+            console.log(users);
+            testHelpers.validateOkResult(users);
+            users.data.length.should.equal(1);
+            done();
+          });
+        });
+      });
+    });
+  });
+});
