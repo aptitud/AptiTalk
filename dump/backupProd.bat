8e9fb0544649c16c36a7abc08b26d3a66ee2d355
Hugo Häggmark <hugo.haggmark@gmail.com>
2014-03-16 08:55:05 +0100

    Changed ip and added some more backup

--- dump/backupProd.bat
+++ dump/backupProd.bat
@@ -1,2 +1,5 @@
 mongodump --host troup.mongohq.com:10037 --db AptiTalk -u admin -pPassw0rd -o C:/git/AptiTalk/dump
 mongorestore -d AptiTalk_Dev C:/git/AptiTalk/dump/AptiTalk
+mongorestore -d AptiTalk_Prod C:/git/AptiTalk/dump/AptiTalk
+
+PAUSE
