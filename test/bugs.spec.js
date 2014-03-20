7cc85c64ea2aa0f99224b426b6ada8dff324e246
Hugo Häggmark <hugo.haggmark@gmail.com>
2014-03-20 18:11:04 +0100

    Fixes #30

--- test/bugs.spec.js
+++ test/bugs.spec.js
@@ -1,5 +1,6 @@
 var should = require('should');
 var chat = require('../lib/chat.js');
+var google = require('../lib/google.js');
 var testHelpers = require("./dbAccess/testHelpers.js");
 
 describe('All bugs', function () {
@@ -40,4 +41,25 @@ describe('All bugs', function () {
 
   });
 
+  describe('Bug #30', function () {
+
+    it('a non aptitud user should not be able to login to AptiTalk', function (done) {
+      var email = 'mega.hacker.aptitud.se@gmail.com';
+      google.verifyUser(email, function (err, isVerified) {
+        should.exists(err);
+        err.should.equal("You need to be a valid Aptitud user");
+        isVerified.should.be.equal(false);
+        done();
+      });
+    });
+
+    it('an aptitud user should be able to login to AptiTalk', function (done) {
+      var email = 'hugo.haggmark@aptitud.se';
+      google.verifyUser(email, function (err, isVerified) {
+        should.not.exists(err);
+        isVerified.should.be.equal(true);
+        done();
+      });
+    });
+  });
 });
